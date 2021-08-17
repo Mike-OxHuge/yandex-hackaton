da95463181f853cbf7b66d0b0f17d2907ee47600
Mike-OxHuge <mikelitoris34@icloud.com>
2021-08-17 22:42:05 +0200

    education mockup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,54 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    <v-container class="text-center">
+      <NuxtLink to="/"> <h1>Yandex.dosug</h1> </NuxtLink>
+      <v-row>
+        <v-col v-for="link in links" :key="link.i" sm="6" md="3">
+          <NuxtLink :to="link.url">{{ link.title }}</NuxtLink>
+        </v-col>
+      </v-row>
+    </v-container>
+    <Nuxt />
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
+      links: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          title: 'Образование',
+          url: '/education',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          title: 'Творчество',
+          url: '/art',
+        },
+        {
+          title: 'Спорт',
+          url: '/sport',
+        },
+        {
+          title: 'Комната отдыха',
+          url: '/chillout-room',
+        },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
     }
-  }
+  },
 }
 </script>
+
+<style>
+a {
+  text-decoration: none;
+}
+/* home route and active route will show in bold as it matches / and /about */
+a.nuxt-link-active {
+  color: #00c58e !important;
+}
+/* exact link will show the primary color for only the exact matching link */
+a.nuxt-link-exact-active {
+  color: #00c58e !important;
+}
+</style>
---
+++ pages/Education.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-container>
+    <h1>Образование</h1>
+    <AdultOrChild @adult="isAdult = true" @child="isAdult = false" />
+    <v-row>
+      <v-col cols="4">
+        <v-row v-if="isAdult">
+          <v-col
+            v-for="action in adultActivities"
+            :key="action.i"
+            cols="12"
+            @click="anythingSelected = true"
+          >
+            <NuxtLink :to="action.url">{{ action.title }}</NuxtLink>
+          </v-col>
+        </v-row>
+        <v-row v-else>
+          <v-col
+            v-for="action in childActivities"
+            :key="action.i"
+            cols="12"
+            @click="anythingSelected = true"
+          >
+            <NuxtLink :to="action.url">{{ action.title }}</NuxtLink>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col cols="8">
+        <NuxtChild v-if="anythingSelected" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AdultOrChild from '~/components/AdultOrChild.vue'
+export default {
+  components: { AdultOrChild },
+  data() {
+    return {
+      anythingSelected: false,
+      isAdult: true,
+      adultActivities: [
+        {
+          title: 'Дизайн',
+          url: '/education/design',
+        },
+        {
+          title: 'Маркетинг',
+          url: '/education/marketing',
+        },
+        {
+          title: 'Программирование',
+          url: '/education/programming',
+        },
+        {
+          title: 'Финансовая грамотность',
+          url: '/education/finance',
+        },
+      ],
+      childActivities: [
+        {
+          title: 'Робототехника',
+          url: '/education/robotics',
+        },
+        {
+          title: 'Программирование',
+          url: '/education/programming',
+        },
+        {
+          title: 'Проектная деятельность',
+          url: '/education/project',
+        },
+        {
+          title: 'Животный мир',
+          url: '/education/animal',
+        },
+        {
+          title: 'История искусств',
+          url: '/education/art',
+        },
+      ],
+    }
+  },
+  methods: {
+    //
+  },
+}
+</script>
+
+<style></style>
